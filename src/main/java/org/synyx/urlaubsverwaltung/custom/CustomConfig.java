2cfbf8c02c62e171c93363c17f9a585d2a965858
maximilian.radmacher <maximilian.radmacher@i-telligence.de>
2025-08-13 11:48:09 +0200

    removed custom config

--- src/main/java/org/synyx/urlaubsverwaltung/custom/CustomConfig.java
+++ src/main/java/org/synyx/urlaubsverwaltung/custom/CustomConfig.java
@@ -1,53 +1,53 @@
-package org.synyx.urlaubsverwaltung.custom;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-
-import java.util.HashMap;
-
-@Configuration
-public class CustomConfig {
-
-    private static final String CLIENT_ID = "itelligence"; // your registration id
-
-    private final ItelligenceAuthSettings itlligenceAuthSettings;
-
-    @Autowired
-    public CustomConfig(ItelligenceAuthSettings itlligenceAuthSettings) {
-        this.itlligenceAuthSettings = itlligenceAuthSettings;
-    }
-
-
-    @Bean
-    ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties props) {
-        var r = props.getRegistration().get(CLIENT_ID);           // your registration id
-        var p = props.getProvider().get(r.getProvider());
-
-        var metadata = new HashMap<String, Object>();
-        metadata.put("end_session_endpoint", itlligenceAuthSettings.getEndSessionUri());
-
-        var reg = ClientRegistration.withRegistrationId(CLIENT_ID)
-            .clientId(r.getClientId())
-            .clientSecret(r.getClientSecret())
-            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-            .redirectUri(r.getRedirectUri())
-            .scope(r.getScope())
-            .authorizationUri(p.getAuthorizationUri())
-            .tokenUri(p.getTokenUri())
-            .userInfoUri(p.getUserInfoUri())
-            .userNameAttributeName(p.getUserNameAttribute())
-            .jwkSetUri(p.getJwkSetUri())
-            .providerConfigurationMetadata(metadata) // <-- inject end_session_endpoint
-            .build();
-
-        return new InMemoryClientRegistrationRepository(reg);
-    }
-
-}
+// package org.synyx.urlaubsverwaltung.custom;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.oauth2.client.registration.ClientRegistration;
+// import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+// import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+// import org.springframework.security.oauth2.core.AuthorizationGrantType;
+//
+// import java.util.HashMap;
+//
+// @Configuration
+// public class CustomConfig {
+//
+//     private static final String CLIENT_ID = "itelligence"; // your registration id
+//
+//     private final ItelligenceAuthSettings itlligenceAuthSettings;
+//
+//     @Autowired
+//     public CustomConfig(ItelligenceAuthSettings itlligenceAuthSettings) {
+//         this.itlligenceAuthSettings = itlligenceAuthSettings;
+//     }
+//
+//
+//     @Bean
+//     ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties props) {
+//         var r = props.getRegistration().get(CLIENT_ID);           // your registration id
+//         var p = props.getProvider().get(r.getProvider());
+//
+//         var metadata = new HashMap<String, Object>();
+//         metadata.put("end_session_endpoint", itlligenceAuthSettings.getEndSessionUri());
+//
+//         var reg = ClientRegistration.withRegistrationId(CLIENT_ID)
+//             .clientId(r.getClientId())
+//             .clientSecret(r.getClientSecret())
+//             .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//             .redirectUri(r.getRedirectUri())
+//             .scope(r.getScope())
+//             .authorizationUri(p.getAuthorizationUri())
+//             .tokenUri(p.getTokenUri())
+//             .userInfoUri(p.getUserInfoUri())
+//             .userNameAttributeName(p.getUserNameAttribute())
+//             .jwkSetUri(p.getJwkSetUri())
+//             .providerConfigurationMetadata(metadata) // <-- inject end_session_endpoint
+//             .build();
+//
+//         return new InMemoryClientRegistrationRepository(reg);
+//     }
+//
+// }
--- src/main/java/org/synyx/urlaubsverwaltung/custom/ItelligenceAuthSettings.java
+++ src/main/java/org/synyx/urlaubsverwaltung/custom/ItelligenceAuthSettings.java
@@ -1,64 +1,64 @@
-package org.synyx.urlaubsverwaltung.custom;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix = "itelligence.auth")
-public class ItelligenceAuthSettings {
-
-    private String baseUri;
-    private String userInfoUri;
-    private String authorizationUri;
-    private String tokenUri;
-    private String jwkSetUri;
-    private String endSessionUri;
-
-    public String getBaseUri() {
-        return baseUri;
-    }
-
-    public void setBaseUri(String baseUri) {
-        this.baseUri = baseUri;
-    }
-
-    public String getUserInfoUri() {
-        return userInfoUri;
-    }
-
-    public void setUserInfoUri(String userInfoUri) {
-        this.userInfoUri = userInfoUri;
-    }
-
-    public String getAuthorizationUri() {
-        return authorizationUri;
-    }
-
-    public void setAuthorizationUri(String authorizationUri) {
-        this.authorizationUri = authorizationUri;
-    }
-
-    public String getTokenUri() {
-        return tokenUri;
-    }
-
-    public void setTokenUri(String tokenUri) {
-        this.tokenUri = tokenUri;
-    }
-
-    public String getJwkSetUri() {
-        return jwkSetUri;
-    }
-
-    public void setJwkSetUri(String jwkSetUri) {
-        this.jwkSetUri = jwkSetUri;
-    }
-
-    public String getEndSessionUri() {
-        return endSessionUri;
-    }
-
-    public void setEndSessionUri(String endSessionUri) {
-        this.endSessionUri = endSessionUri;
-    }
-}
+// package org.synyx.urlaubsverwaltung.custom;
+//
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.Configuration;
+//
+// @Configuration
+// @ConfigurationProperties(prefix = "itelligence.auth")
+// public class ItelligenceAuthSettings {
+//
+//     private String baseUri;
+//     private String userInfoUri;
+//     private String authorizationUri;
+//     private String tokenUri;
+//     private String jwkSetUri;
+//     private String endSessionUri;
+//
+//     public String getBaseUri() {
+//         return baseUri;
+//     }
+//
+//     public void setBaseUri(String baseUri) {
+//         this.baseUri = baseUri;
+//     }
+//
+//     public String getUserInfoUri() {
+//         return userInfoUri;
+//     }
+//
+//     public void setUserInfoUri(String userInfoUri) {
+//         this.userInfoUri = userInfoUri;
+//     }
+//
+//     public String getAuthorizationUri() {
+//         return authorizationUri;
+//     }
+//
+//     public void setAuthorizationUri(String authorizationUri) {
+//         this.authorizationUri = authorizationUri;
+//     }
+//
+//     public String getTokenUri() {
+//         return tokenUri;
+//     }
+//
+//     public void setTokenUri(String tokenUri) {
+//         this.tokenUri = tokenUri;
+//     }
+//
+//     public String getJwkSetUri() {
+//         return jwkSetUri;
+//     }
+//
+//     public void setJwkSetUri(String jwkSetUri) {
+//         this.jwkSetUri = jwkSetUri;
+//     }
+//
+//     public String getEndSessionUri() {
+//         return endSessionUri;
+//     }
+//
+//     public void setEndSessionUri(String endSessionUri) {
+//         this.endSessionUri = endSessionUri;
+//     }
+// }
